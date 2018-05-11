4c36a1b0c0932fb9822ca69dd748a7bedda760e9
wallandall <wallandall@gmail.com>
2018-05-11 12:38:04 +0000

    Added Devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,5 +10,19 @@ class HomeController < ApplicationController
   end
   
   def lookup
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coin = JSON.parse(@response)
+  
+    @symbol = params[:sym]
+    if @symbol == ""
+      @symbol = "Please enter a valid currency!"
+    end
+    if @symbol
+      @symbol = @symbol.upcase
+    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "crytofolio_#{Rails.env}"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'coin-tracker.webe-solutions.com', :protocol => 'https'}
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
